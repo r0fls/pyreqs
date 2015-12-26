b600870cb64435b2ce6950bdf2f49eafaab695d0
r0fls <raphael.deem@gmail.com>
2015-12-25 19:21:34 -0800

    pep8

--- get_reqs.py
+++ get_reqs.py
@@ -8,17 +8,30 @@ import sys
 # - write to requirements.txt
 # - ignore more builtin modules
 
-OTHER_BUILTINS = ['os','urllib2', 'collections', 'multiprocessing','subprocess','distutils','json','functools','dateutil','datetime']
+OTHER_BUILTINS = [
+    'os',
+    'urllib2',
+    'collections',
+    'multiprocessing',
+    'subprocess',
+    'distutils',
+    'json',
+    'functools',
+    'dateutil',
+    'datetime']
 BUILTINS = set(sys.builtin_module_names).union(OTHER_BUILTINS)
+
 
 def dir_iter(location):
     files = []
     for (dirpath, dirnames, filenames) in walk(location):
-        files.extend(map(lambda x: join(dirpath,x), filter(lambda x: not x.startswith('.'), filenames)))
+        files.extend(map(lambda x: join(dirpath, x), filter(
+            lambda x: not x.startswith('.'), filenames)))
     return files
 
+
 def get_reqs(location):
-    if isinstance(location,file):
+    if isinstance(location, file):
         return get_reqs_file(location)
     if isinstance(location, basestring):
         reqs = set()
@@ -31,6 +44,7 @@ def get_reqs(location):
                 reqs = reqs.union(get_reqs_file(f))
         return list(reqs)
 
+
 def get_reqs_file(f):
     reqs = set()
     module = f.read().split('\n')
@@ -38,24 +52,29 @@ def get_reqs_file(f):
         reqs_list = []
         if 'import' == line.strip()[:6]:
             if line.find(',') > 0:
-                req = [i.strip().split('.')[0] for i in line.replace('import ','').strip().split(',')]
+                req = [i.strip().split('.')[0]
+                       for i in line.replace('import ', '').strip().split(',')]
             else:
                 if line.find(' as ') > 0:
-                    req = line[line.find('import')+7:line.find(' as')].split('.')[0].strip()
+                    req = line[
+                        line.find('import') +
+                        7:line.find(' as')].split('.')[0].strip()
                 else:
-                    req = line.replace('import','').strip()
+                    req = line.replace('import', '').strip()
         elif 'from' == line.strip()[:4]:
-            req = line[line.find('from')+4:line.find('import')].strip().split('.')[0]
+            req = line[
+                line.find('from') +
+                4:line.find('import')].strip().split('.')[0]
         else:
             continue
-        if type(req) == str:
+        if isinstance(req, str):
             if req not in BUILTINS:
                 reqs.add(req)
-        elif type(req) == list:
+        elif isinstance(req, list):
             for r in req:
                 if r not in BUILTINS:
                     reqs.add(r)
     return reqs
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print get_reqs(sys.argv[1])
